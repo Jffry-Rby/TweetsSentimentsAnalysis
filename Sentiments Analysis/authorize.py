ca5c80852c988a3a0273dde0d0459867fcc572b4
Jeffry Rubaya <jeffjip19@gmail.com>
2020-04-06 01:39:05 +0800

    Moving autohrize

---
+++ Sentiments Analysis/authorize.py
@@ -0,0 +1,6 @@
+auth_params = {
+    'app_key':'',
+    'app_secret':'',
+    'oauth_token':'',
+    'oauth_token_secret':''
+}
--- authorize.py
+++
@@ -1,6 +0,0 @@
-auth_params = {
-    'app_key':'',
-    'app_secret':'',
-    'oauth_token':'',
-    'oauth_token_secret':''
-}
